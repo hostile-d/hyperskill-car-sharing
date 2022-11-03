9746d47fdf6e29116e15bad3d8a1635667b812f6
hostile-d <host.chel@gmail.com>
2022-11-03 21:27:28 +0400

    stage 2

--- Car Sharing/task/src/carsharing/CLI.java
+++ Car Sharing/task/src/carsharing/CLI.java
@@ -5,7 +5,6 @@ import java.util.concurrent.Callable;
 
 public class CLI {
     final String DB_FILE_NAME_ARG = "-databaseFileName";
-    final Integer BACK_OPTION = 0;
     private MenuNode menu = new MenuNode();
     private MenuNode currentMenuNode;
     private Scanner scanner = new Scanner(System.in);
@@ -24,56 +23,58 @@ public class CLI {
         }
         return  dbFileName;
     }
-    public void startNavigation() {
-        printCurrentMenu();
-        handleMenuChange(scanner.nextInt());
-    }
     public void setDbManager(DBManager dbManager) {
         this.dbManager = dbManager;
     }
-    private void handleMenuChange(Integer selectedOption) {
-        MenuNode nextNode;
-        if (Objects.equals(selectedOption, BACK_OPTION)) {
-            nextNode = currentMenuNode.getParent();
-        } else {
-            nextNode = currentMenuNode.children.get(selectedOption);
-            if (Objects.equals(nextNode, null)) {
-                System.out.println("Incorrect input");
-                return;
-            }
-            try {
-                nextNode.callAction();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+    public void startNavigation() {
+        printCurrentMenu();
+        var nextChoise = scanner.nextLine();
+        try {
+            handleMenuChange(Integer.parseInt(nextChoise));
+        } catch (NumberFormatException e) {
+            System.out.printf(
+                    "Incorrect input, type a number from 0 to %s to continue\n\n",
+                    currentMenuNode.getChildren().size() - 1
+            );
+            startNavigation();
         }
-        currentMenuNode = nextNode;
+    }
+    private void handleMenuChange(Integer selectedOption) {
+        var nextNode = currentMenuNode.getNextNode(selectedOption);
+
+        if (Objects.equals(nextNode, null)) {
+            scanner.close();
+            return;
+        }
+
+        callAction(nextNode);
+
+        if (!nextNode.getChildren().isEmpty()) {
+            currentMenuNode = nextNode;
+        }
+
         startNavigation();
     }
-
+    private void callAction(MenuNode nextNode) {
+        try {
+            nextNode.callAction();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
     private void printCurrentMenu() {
         if (Objects.equals(currentMenuNode, null)) {
             return;
         }
-        currentMenuNode.children.forEach((id, node) -> System.out.println(node));
+        currentMenuNode.getChildren().forEach((id, node) -> System.out.println(node));
     }
-
     private void createMenu() {
-        menu.addChild(new MenuNode(1, "Log in as a manager", this::loginAsManager));
-        menu.addChild(new MenuNode(0, "Exit", this::exit));
+        menu.addChild(new MenuNode(1, "Log in as a manager"));
+        menu.addChild(new MenuNode(0, "Exit"));
 
-        menu.children.get(1).addChild(new MenuNode(1, "Company list", this::printCompanyList));
-        menu.children.get(1).addChild(new MenuNode(2, "Create a company", this::createCompany));
-        menu.children.get(1).addChild(new MenuNode(0, "Back", this::getBack));
-    }
-    public Callable loginAsManager() {
-        return null;
-    }
-    public Callable exit() {
-        return null;
-    }
-    public Callable getBack() {
-        return null;
+        menu.getChildren().get(1).addChild(new MenuNode(1, "Company list", this::printCompanyList));
+        menu.getChildren().get(1).addChild(new MenuNode(2, "Create a company", this::createCompany));
+        menu.getChildren().get(1).addChild(new MenuNode(0, "Back"));
     }
     public Callable printCompanyList() {
         var companies = dbManager.getCompanies();
@@ -84,11 +85,15 @@ public class CLI {
                 System.out.println((i + 1) + ". " + companies.get(i));
             }
         }
+        System.out.println();
         return null;
     }
     public Callable createCompany() {
         System.out.println("Enter the company name:");
-        dbManager.creteCompany(scanner.nextLine());
+        String name = scanner.nextLine();
+        dbManager.creteCompany(name);
+        System.out.println("The company was created!");
+        System.out.println();
         return null;
     }
 }
--- Car Sharing/task/src/carsharing/MenuNode.java
+++ Car Sharing/task/src/carsharing/MenuNode.java
@@ -1,19 +1,27 @@
 package carsharing;
 
 import java.util.LinkedHashMap;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 
 public class MenuNode {
-    Integer id;
-    String name;
-    MenuNode parent;
-    LinkedHashMap<Integer, MenuNode> children;
-    Callable<?> action;
+    private Integer id;
+    private String name;
+    private MenuNode parent;
+    private LinkedHashMap<Integer, MenuNode> children;
+    private Callable<?> action = null;
+    final private Integer BACK_OPTION = 0;
+
     public MenuNode(Integer id, String name, Callable<?> action) {
         this.id = id;
         this.name = name;
         this.children = new LinkedHashMap<Integer, MenuNode>();
         this.action = action;
+    }
+    public MenuNode(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+        this.children = new LinkedHashMap<Integer, MenuNode>();
     }
     private void setParent(MenuNode node) {
         this.parent = node;
@@ -24,17 +32,32 @@ public class MenuNode {
     public MenuNode getParent() {
         return parent;
     }
+
+    public LinkedHashMap<Integer, MenuNode> getChildren() {
+        return children;
+    }
     public MenuNode() {
         this.id = null;
         this.name = null;
         this.children = new LinkedHashMap<Integer, MenuNode>();
+    }
+    public MenuNode getNextNode(Integer next) {
+        MenuNode nextNode;
+        if (Objects.equals(next, BACK_OPTION)) {
+            nextNode = this.getParent();
+        } else {
+            nextNode = this.children.get(next);
+        }
+        return nextNode;
     }
     public void addChild(MenuNode child) {
         children.put(child.id, child);
         child.setParent(this);
     }
     public void callAction() throws Exception {
-        this.action.call();
+        if (!Objects.equals(this.action, null)) {
+            this.action.call();
+        }
     }
     @Override
     public String toString() {
